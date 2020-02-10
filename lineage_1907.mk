daf89f44dd8c9cbeb8a6cd9dddff0a969e1f860d
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-11 01:31:35 +0530

    full_k68v1_64-user-9-PPR1.180610.011-compiler12191809-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_1907.mk
+++ lineage_1907.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := vivo 1907
 PRODUCT_GMS_CLIENTID_BASE := android-vivo
 TARGET_VENDOR := vivo
 TARGET_VENDOR_PRODUCT_NAME := 1907
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="full_k68v1_64-user 9 PPR1.180610.011 compiler11281608 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="full_k68v1_64-user 9 PPR1.180610.011 compiler12191809 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := vivo/1907/1907:9/PPR1.180610.011/compiler11281608:user/release-keys
+BUILD_FINGERPRINT := vivo/1907/1907:9/PPR1.180610.011/compiler12191809:user/release-keys
